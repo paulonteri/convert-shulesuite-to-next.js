d47209d9d779bea01690ef7f6d9396012b988624
Paul Onteri <paul@inuua.net>
2021-01-27 17:39:27 +0300

    progress

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,14 @@ import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.css";
 
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
 
