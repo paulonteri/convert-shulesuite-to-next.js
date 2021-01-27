d93575b9fa552ec9ab1d45209ce43ca598e4641f
Paul Onteri <paul@inuua.net>
2021-01-27 21:46:45 +0300

    add layout files

---
+++ layout/Dashboard.js
@@ -0,0 +1,101 @@
+import React, { Suspense, useState } from "react";
+import { Layout } from "antd";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import SideContent from "./SideContent";
+import AppFooter from "./AppFooter";
+
+const { Header, Sider, Content } = Layout;
+
+function Dashboard(props) {
+    // State
+    const [collapsed, setCollapsed] = useState(false);
+    const [broken, setBroken] = useState(false);
+
+    const toggle = () => {
+        setCollapsed(!collapsed);
+    };
+
+    const onClick = () => {
+        switch (broken) {
+            case true:
+                setCollapsed(true);
+                break;
+            default:
+                return {};
+        }
+    };
+
+    return (
+        <Layout style={{ height: "100%" }}>
+            <Sider
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+                breakpoint="md"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    switch (broken) {
+                        case true:
+                            setBroken(true);
+                            setCollapsed(true);
+
+                            break;
+                        case false:
+                            setBroken(false);
+                            setCollapsed(false);
+                            break;
+
+                        default:
+                            setBroken(false);
+                            setCollapsed(false);
+                            break;
+                    }
+                }}
+            >
+                <SideContent onClickFunc={onClick} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        margin: 0,
+                        padding: 0,
+                        backgroundColor: "white"
+                    }}
+                >
+                    <div className="pt-0 pl-2">
+                        {collapsed ? (
+                            <MenuUnfoldOutlined
+                                className="trigger"
+                                onClick={toggle}
+                                style={{
+                                    margin: 0,
+                                    padding: 0,
+                                    fontSize: "18px"
+                                }}
+                            />
+                        ) : (
+                            <MenuFoldOutlined
+                                className="trigger"
+                                onClick={toggle}
+                                style={{
+                                    margin: 0,
+                                    padding: 0,
+                                    fontSize: "18px"
+                                }}
+                            />
+                        )}
+                    </div>
+                </Header>
+
+                <Content className="mt-lg-3 mt-md-2 mt-1 px-xl-3 px-lg-2 px-1">
+                    {props.children}
+                </Content>
+
+                <AppFooter />
+            </Layout>
+        </Layout>
+    );
+}
+
+export default Dashboard;
